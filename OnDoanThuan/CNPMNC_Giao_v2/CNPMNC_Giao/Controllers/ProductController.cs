3f65ae1021619321e2c160915e9afad6f7184ec9
odt204 <thuanon11@gmail.com>
2024-10-13 13:46:35 +0700

    OnDoanThuan_Update

---
+++ OnDoanThuan/CNPMNC_Giao_v2/CNPMNC_Giao/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using CNPMNC_Giao.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CNPMNC_Giao.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly CNPMNCEntities db = new CNPMNCEntities();
+        // GET: Product
+        public ActionResult Index()
+        {
+            var danhSachSanPham = db.SanPham.ToList();
+
+            // Truyền danh sách vào View
+            return View(danhSachSanPham);
+        }
+    }
+}
