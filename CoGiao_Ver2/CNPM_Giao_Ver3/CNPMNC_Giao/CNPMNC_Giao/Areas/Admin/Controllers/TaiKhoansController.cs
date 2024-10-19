43205e9d962622c1470d6b540ea5fc5b5c934a69
MonSPen-nnt <monpen.nnt@gmail.com>
2024-10-19 23:53:38 +0700

    cap nhat

---
+++ CoGiao_Ver2/CNPM_Giao_Ver3/CNPMNC_Giao/CNPMNC_Giao/Areas/Admin/Controllers/TaiKhoansController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CNPMNC_Giao.Models;
+
+namespace CNPMNC_Giao.Areas.Admin.Controllers
+{
+    public class TaiKhoansController : Controller
+    {
+        private CNPMNCEntities db = new CNPMNCEntities();
+
+        // GET: Admin/TaiKhoans
+        public ActionResult Index()
+        {
+            return View(db.TaiKhoans.ToList());
+        }
+
+        // GET: Admin/TaiKhoans/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaiKhoan taiKhoan = db.TaiKhoans.Find(id);
+            if (taiKhoan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taiKhoan);
+        }
+
+        // GET: Admin/TaiKhoans/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/TaiKhoans/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaTaiKhoan,Email,MatKhau,VaiTro")] TaiKhoan taiKhoan)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TaiKhoans.Add(taiKhoan);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(taiKhoan);
+        }
+
+        // GET: Admin/TaiKhoans/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaiKhoan taiKhoan = db.TaiKhoans.Find(id);
+            if (taiKhoan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taiKhoan);
+        }
+
+        // POST: Admin/TaiKhoans/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaTaiKhoan,Email,MatKhau,VaiTro")] TaiKhoan taiKhoan)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(taiKhoan).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(taiKhoan);
+        }
+
+        // GET: Admin/TaiKhoans/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaiKhoan taiKhoan = db.TaiKhoans.Find(id);
+            if (taiKhoan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taiKhoan);
+        }
+
+        // POST: Admin/TaiKhoans/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TaiKhoan taiKhoan = db.TaiKhoans.Find(id);
+            db.TaiKhoans.Remove(taiKhoan);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        [HttpPost]
+        public ActionResult ChangeStatus(int id)
+        {
+            var taiKhoan = db.TaiKhoans.Find(id);
+            if (taiKhoan == null)
+            {
+                return HttpNotFound();
+            }
+
+            // Thay đổi trạng thái (true -> false, false -> true)
+            taiKhoan.VaiTro = !taiKhoan.VaiTro;
+
+            // Cập nhật lại database
+            db.Entry(taiKhoan).State = EntityState.Modified;
+            db.SaveChanges();
+
+            // Quay lại trang Details
+            return RedirectToAction("Details", new { id = taiKhoan.MaTaiKhoan });
+        }
+    }
+}
---
+++ CoGiao_Ver2/CNPM_Giao_Ver3/CNPMNC_Giao/CNPMNC_Giao/Controllers/APIContext.cs
@@ -0,0 +1,12 @@
+﻿namespace CNPMNC_Giao.Controllers
+{
+    internal class APIContext
+    {
+        private object value;
+
+        public APIContext(object value)
+        {
+            this.value = value;
+        }
+    }
+}
