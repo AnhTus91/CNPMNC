770cb7a7853a6455aa0d84e126e1587c7d56a24f
Tai2602 <nguyentai4273@gmail.com>
2024-11-01 00:21:27 +0700

    sprint4

---
+++ NguyenPhatTai/CNPMNC_Giao_v3/CNPMNC_Giao/Controllers/GioHangsController.cs
@@ -0,0 +1,72 @@
+﻿using CNPMNC_Giao.Models;
+using Microsoft.AspNetCore.Mvc; // Đảm bảo sử dụng Microsoft.AspNetCore.Mvc
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+public class GioHangsController : Controller
+{
+    private readonly DAPM_1Entities _context;
+
+    public GioHangsController(DAPM_1Entities context)
+    {
+        _context = context;
+    }
+
+    // Thêm sản phẩm vào giỏ hàng
+    [HttpPost]
+    public IActionResult ThemVaoGioHang(int MaSanPham, int SoLuong)
+    {
+        var sanPham = _context.SanPham.Find(MaSanPham);
+        if (sanPham == null)
+        {
+            return NotFound(); // Trả về 404 nếu không tìm thấy sản phẩm
+        }
+
+        // Lấy giỏ hàng của người dùng (giả sử MaNguoiDung = 1)
+        var gioHang = _context.GioHang.Include(g => g.ChiTietGioHang).FirstOrDefault(g => g.MaNguoiDung == 1) ?? new GioHang
+        {
+            MaNguoiDung = 1,
+            NgayTao = DateTime.Now,
+            ChiTietGioHang = new List<ChiTietGioHang>() // Khởi tạo danh sách chi tiết giỏ hàng
+        };
+
+        // Thêm chi tiết vào giỏ hàng
+        var chiTiet = new ChiTietGioHang
+        {
+            MaSanPham = MaSanPham,
+            SoLuong = SoLuong,
+            GiaBan = sanPham.GiaTienMoi,
+            SanPham = sanPham
+        };
+
+        gioHang.ChiTietGioHang.Add(chiTiet);
+        gioHang.TongTien += chiTiet.GiaBan * chiTiet.SoLuong;
+
+        // Nếu giỏ hàng đã tồn tại thì cập nhật, nếu không thì thêm mới
+        if (_context.GioHang.Any(g => g.MaGioHang == gioHang.MaGioHang))
+        {
+            _context.GioHang.Update(gioHang);
+        }
+        else
+        {
+            _context.GioHangs.Add(gioHang);
+        }
+
+        _context.SaveChanges();
+
+        return RedirectToAction("Index");  // Chuyển sang trang giỏ hàng
+    }
+
+    public IActionResult Index()
+    {
+        var gioHang = _context.GioHang.Include(g => g.ChiTietGioHang).FirstOrDefault(g => g.MaNguoiDung == 1);
+        if (gioHang == null)
+        {
+            return NotFound(); // Trả về 404 nếu giỏ hàng không tìm thấy
+        }
+        return View(gioHang);
+    }
+
+}
---
+++ NguyenPhatTai/CNPMNC_Giao_v3/CNPMNC_Giao/Controllers/HomeController.cs
@@ -0,0 +1,100 @@
+﻿using CNPMNC_Giao.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CNPMNC_Giao.Controllers
+{
+    public class HomeController : Controller
+    {
+        private CNPMNC1Entities db = new CNPMNC1Entities();
+        public ActionResult Index()
+        {
+            var sanPhams = db.SanPham.OrderByDescending(sp => sp.NgayTao).ToList();
+            return View(sanPhams);
+        }
+        //public ActionResult Search(string keyword)
+        //{
+        //    // Kiểm tra nếu keyword có giá trị
+        //    if (!string.IsNullOrEmpty(keyword))
+        //    {
+        //        // Tìm kiếm sản phẩm theo tên
+        //        var sanPhams = db.SanPham
+        //                         .Where(sp => sp.TenSanPham.Contains(keyword))
+        //                         .OrderByDescending(sp => sp.NgayTao)
+        //                         .ToList();
+        //        return View("Index", sanPhams); // Trả về danh sách sản phẩm tìm được
+        //    }
+
+        //    // Nếu không có từ khóa, trả về toàn bộ sản phẩm
+        //    return RedirectToAction("Index");
+        //}
+        public JsonResult GetData()
+        {
+            int cartCount = 0;
+            if (Session["ShoppingCart"] != null) // Nếu giỏ hàng chưa được khởi tạo
+            {
+                List<ChiTietGioHang> ShoppingCart = Session["ShoppingCart"] as List<ChiTietGioHang>;
+                cartCount = ShoppingCart.Count();
+
+            }
+
+            return Json(cartCount, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult DonHang()
+        {
+            // Kiểm tra session đăng nhập
+            if (Session["userLogin"] == null)
+            {
+                return RedirectToAction("Login", "User");
+            }
+            else
+            {
+                // Kiểm tra và chuyển đổi giá trị từ session thành kiểu int
+                if (int.TryParse(Session["userLogin"].ToString(), out int userID))
+                {
+                    // Truy vấn đơn hàng của người dùng
+                    var donhang = db.GioHang
+                                    .Where(dh => dh.MaNguoiDung == userID)
+                                    .Include(dh => dh.ChiTietGioHangs) // Sử dụng lambda thay vì chuỗi
+                                    .OrderByDescending(e => e.TrangThai)
+                                    .ToList();
+
+                    return View(donhang);
+                }
+                else
+                {
+                    // Nếu không thể chuyển đổi về int, chuyển hướng về trang đăng nhập
+                    return RedirectToAction("Login", "User");
+                }
+            }
+        }
+
+
+
+        public RedirectToRouteResult HuyDonHang(string maDH)
+        {
+            if (Session["userLogin"] == null)
+            {
+                return RedirectToAction("Login", "User");
+            }
+            else
+            {
+                int userID = (int)Session["userLogin"];
+                var dh = from donhang in db.DonHang
+                         where donhang.MaDonHang == maDH && donhang.MaNguoiGui == userID
+                         select donhang;
+                dh.FirstOrDefault().NgayDatHang = DateTime.Now;
+                dh.FirstOrDefault().TrangThai = "Đã hủy";
+                db.SaveChanges();
+                return RedirectToAction("DonHang", "Home");
+            }
+
+
+        }
+    }
+}
